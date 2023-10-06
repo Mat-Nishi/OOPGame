377eff300a5835664746a8a23d44e6aa65dc9f8a
Mateus Nishimura <mateusnishimura@gmail.com>
2023-10-06 17:22:42 -0300

    Added functions to draw and move player

--- game.py
+++ game.py
@@ -1,30 +1,44 @@
 import pygame
+import os
 
 class Window:
-    def __init__(self,h,w) -> None:
+    def __init__(self,h,w,bgColor=(0,0,0)) -> None:
         self._height = h
         self._width = w
+        self.bgColor = bgColor
         self._window = pygame.display.set_mode((h,w))
     
     def setBG(self, color):
+        self.bgColor = color
         self._window.fill(color)
+        pygame.display.update()
+    
+    def clear(self):
+        self._window.fill(self.bgColor)
+    
+    def drawImage(self, image, x, y):
+        self._window.blit(image, (x,y))
         pygame.display.update()
         
 class Player():
-    def __init__(self, sprite) -> None:
-        self._body = sprite
+    def __init__(self, h, w, speed, sprite) -> None:
+        self._height = h
+        self._width = w
+        self.speed = speed
+        img = pygame.image.load(os.path.join("Assets",sprite))
+        self._body = pygame.transform.scale(img, (self._height, self._width))
         self.posX = 0
         self.posY = 0
     
-    def move(self, dir, speed):
+    def move(self, dir):
         if dir == 'up':
-            self.posY -= speed
+            self.posY -= self.speed
         elif dir == 'down':
-            self.posY += speed
+            self.posY += self.speed
         elif dir == 'left':
-            self.posX -= speed
+            self.posX -= self.speed
         else:
-            self.posX += speed
+            self.posX += self.speed
     
     def setPos(self, x,y):
         self.posX = x
@@ -32,12 +46,26 @@ class Player():
 
 
 class Engine:
-    def __init__(self, window, fps=60) -> None:
+    def __init__(self, window, player, fps=60) -> None:
         self.window = window
         self._fps = fps
         self._clock = pygame.time.Clock()
         self._state = "game"
-        
+        self._player = player
+
+    def drawPlayer(self):
+        self.window.drawImage(self._player._body, self._player.posX, self._player.posY)
+    
+    def playerMovement(self):
+        pressed_keys = pygame.key.get_pressed()
+        if pressed_keys[pygame.K_w]: #Up
+            self._player.move('up')
+        if pressed_keys[pygame.K_a]: #Left
+            self._player.move('left')
+        if pressed_keys[pygame.K_s]: #Down
+            self._player.move('down')
+        if pressed_keys[pygame.K_d]: #Right
+            self._player.move('right')
 
     def runGame(self):
         while self._state == "game":
@@ -47,6 +75,9 @@ class Engine:
                 if event.type == pygame.QUIT:
                     self._state = "menu"
             # self.window.setBG((255,0,0))
+            self.window.clear()
+            self.playerMovement()
+            self.drawPlayer()
 
         pygame.quit()
 
@@ -54,7 +85,8 @@ class Engine:
 
 def main():
     window = Window(500,500)
-    game = Engine(window)
+    player = Player(140,60,5,"crab.png")
+    game = Engine(window, player)
     game.runGame()
 
 if __name__ == "__main__":
